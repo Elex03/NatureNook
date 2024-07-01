28f4b3e2243d38931052c17641bba32dd0bf53e9
soofieez <anas.ordonez@gmail.com>
2024-06-30 22:21:21 -0600

    changes menu

--- src/Engine/finished/ButtonCreator.py
+++ src/Engine/finished/ButtonCreator.py
@@ -6,9 +6,9 @@ class ButtonCreator:
 
     def create_buttons(self):
         # Main menu buttons
-        self.button_manager.create_button("resume", 304, 125, "button_resume.png", 1, "main")
-        self.button_manager.create_button("options", 297, 250, "button_options.png", 1, "main")
-        self.button_manager.create_button("quit", 290, 375, "button_quit.png", 1, "main")
+        self.button_manager.create_button("resume", 50, 200, "button_resume.png", 1, "main")
+        self.button_manager.create_button("options", 43, 325, "button_options.png", 1, "main")
+        self.button_manager.create_button("quit", 36, 450, "button_quit.png", 1, "main")
 
         # Options menu buttons
         self.button_manager.create_button("audio", 225, 200, "button_audio.png", 1, "options")
